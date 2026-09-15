2385bd5e77f4ba3d6238a46301850941b04bd61c
Mike-Mclean <mmcle009@gmail.com>
2026-09-15 10:47:35 -0400

    Refactored handle_get and removed the function for handle_ping

--- src/command_handlers.cpp
+++ src/command_handlers.cpp
@@ -20,7 +20,7 @@ std::string handle_received(respInput& parsed_command, Datastore& data)
   } else if (command.type == "GET") {
     response = handle_get(command.args, data);
   } else if (command.type == "PING"){
-    response = handle_ping();
+    response = "+PONG\r\n";
   } else {
     throw std::runtime_error{"Unknown command"};
   }
@@ -54,18 +54,23 @@ std::string handle_set(const std::vector<std::string>& set_args, Datastore& data
   return "+OK\r\n";
 }
 
-std::string handle_get(const std::vector<std::string>& key_details, Datastore& data)
+std::string handle_get(const std::vector<std::string>& get_args, Datastore& data)
 {
-  std::string full_key {key_details[0]};
-  for (size_t kd_idx {1}; kd_idx < key_details.size(); kd_idx++)
-    full_key += " " + key_details[kd_idx];
+  if (get_args.size() > 1)
+    throw std::runtime_error{"Too many arguments to get command"};
 
-  if (auto map_value = data.get_map_value(full_key)){
+  std::string key {get_args[0]};
+
+  if (auto map_value = data.get_map_value(key)){
+
     std::string response { "$" };
     response += std::to_string(map_value->length()) + "\r\n" + map_value.value() + "\r\n";
     return response;
+
   } else {
+
     return "$-1\r\n";
+
   }
 }
 
--- src/command_handlers.h
+++ src/command_handlers.h
@@ -20,7 +20,6 @@ std::string handle_echo(const std::string& echo_message);
 std::string handle_set(const std::vector<std::string>& pair_details, Datastore& data);
 std::string handle_get(const std::vector<std::string>& key_details, Datastore& data);
 std::string handle_received(std::vector<std::string>& parsed_received_message, Datastore& data);
-std::string handle_ping();
 
 
 redisCommand extract_command(respInput& parsed_input);
